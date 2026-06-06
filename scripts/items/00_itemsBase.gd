77a2bff4843869ed5d6101919d181e85c7c46b41
Mantequillion <scribble14discord@gmail.com>
2026-06-06 18:09:39 +0200

    actualisasiones y also añadí el input context ig
    cositas del baseitemwhatever

--- scripts/items/00_itemsBase.gd
+++ scripts/items/00_itemsBase.gd
@@ -2,11 +2,11 @@ extends Area2D
 class_name customItem
 
 var itemID: int = 0
-
+var itemName = "baseItem"
 
 func _input_event(viewport, event, shape_idx):
 	if event.is_action_pressed("IA_click"):
-		print("click!!!!")
-		print(itemID)
+		print(itemName)
+		
 func _on_mouse_entered():
 	print(itemID)
--- scripts/items/01_cuerda.gd
+++ scripts/items/01_cuerda.gd
@@ -2,7 +2,8 @@ extends customItem
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	itemID = 1 # Replace with function body.
+	itemID = 1
+	itemName = "cuerda"
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
