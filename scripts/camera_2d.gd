3639e1853abd2ccc6ef19e05dc2b989fc3060eaf
DonuT_ell <danimine24@gmail.com>
2026-06-06 13:52:12 +0200

    :sparkles: Camera moving up and down
    It is inside the puzzle scene!! (I should isolate the camera scene, now that I think of it and instantiate it into the puzzle)

---
+++ scripts/camera_2d.gd
@@ -0,0 +1,41 @@
+extends Camera2D
+
+@export var speed: int = 300
+var movingDown: bool = false
+var movingUp: bool = false
+
+func _ready():
+	pass 
+
+
+func _process(_delta):
+	cameraScrollDown()
+	cameraScrollUp()
+
+
+func _on_scroll_down_area_mouse_entered():
+	movingDown = true
+
+
+func _on_scroll_down_area_mouse_exited():
+	movingDown = false
+
+
+func _on_scroll_up_area_mouse_entered():
+	movingUp = true
+
+
+func _on_scroll_up_area_mouse_exited():
+	movingUp = false
+
+
+func cameraScrollDown():
+	if not movingDown:
+		return
+	position.y += speed * get_process_delta_time()
+
+
+func cameraScrollUp():
+	if not movingUp:
+		return
+	position.y -= speed * get_process_delta_time()
