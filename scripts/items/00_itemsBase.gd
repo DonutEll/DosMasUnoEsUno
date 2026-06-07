89e7a69758912e818cd202336fd5f864089bbda9
Mantequillion <scribble14discord@gmail.com>
2026-06-07 13:59:32 +0200

    se manda señal!!!
    PORrefin se manda la señalsita al arrray jeje
    jejeje
    y actualizadas cositas

--- scripts/items/00_itemsBase.gd
+++ scripts/items/00_itemsBase.gd
@@ -4,9 +4,12 @@ class_name customItem
 var itemID: int = 0
 var itemName = "baseItem"
 
+signal IDtransfer(itemID)
+
 func _input_event(viewport, event, shape_idx):
 	if event.is_action_pressed("IA_click"):
 		print(itemName)
+		emit_signal("IDtransfer", itemID)
 		
 func _on_mouse_entered(): #recordar conectar señal!!
 	print("wah")
