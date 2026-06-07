a0be0888f160dbd9571f57036e9e0e5736c6bec0
DonuT_ell <danimine24@gmail.com>
2026-06-07 12:22:40 +0200

    :ambulance: Hotfix proyecto
    ARREGLADO!!!

--- scripts/gnome_puzzles.gd
+++ scripts/gnome_puzzles.gd
@@ -1,33 +1,11 @@
 extends Node2D
-@export var thisSceneCamera: puzzleCamera 
-@export var roundRequirements: Array = [[1], [2], [3], [4, 5], [6, 7], [6,3], [2, 8, 6], [9, 1, 7], [3, 6, 10], [6, 8, 9, 5]]       
-var currentRound: int = 0
-var currentRoundSubmission: Array = []
-var pulsos: int = 0
+@export var currentRound: String = "Ronda 1"
 
 
 func _ready():
-	updateRoundLabel()
-	thisSceneCamera.endRound.connect(endRound)
+	pass 
 
 
 
 func _process(delta):
 	pass
-
-
-func endRound(text):
-	currentRoundSubmission.append(text)
-	print(currentRoundSubmission)
-	$FondoSinTerminar/HandAnimation.play("MANO")
-	if not (currentRoundSubmission == roundRequirements[currentRound]):
-		printerr("Ronda fallida, reiniciar la ronda")
-		return
-	printerr("Ronda exitosa, avanzar a la siguiente ronda")
-	currentRound += 1
-	print(text)
-	updateRoundLabel()
-
-
-func updateRoundLabel():
-	$Camera2D/RoundLabel.set_text("Ronda: " + str(currentRound + 1))
